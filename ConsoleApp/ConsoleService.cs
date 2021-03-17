1e6041f867c798d49c6e520a8471df741106da7e
45software <50671522+45software@users.noreply.github.com>
2021-03-17 13:54:01 -0500

    Simplified options member variable
    Update to store a reference to a CustomOptions object rather than an IOptions<CustomOptions> to simplify usage of the options.

--- ConsoleApp/ConsoleService.cs
+++ ConsoleApp/ConsoleService.cs
@@ -8,21 +8,21 @@ namespace ConsoleApp
 	public class ConsoleService : IConsoleService
 	{
 		private readonly ILogger<ConsoleService> _logger;
-		private readonly IOptions<CustomOptions> _options;
+		private readonly CustomOptions _options;
 
 
 		public ConsoleService(ILogger<ConsoleService> logger,
 									IOptions<CustomOptions> options)
 		{
 			_logger = logger;
-			_options = options;
+			_options = options.Value;
 		}
 
 
 		public void Run(string[] args)
 		{
 			_logger.LogInformation("The application is now starting {Start}", DateTime.Now);
-			Console.WriteLine($"The Url in the settings file is '{_options.Value.Url}'");
+			Console.WriteLine($"The Url in the settings file is '{_options.Url}'");
 		}
 	}
 }
