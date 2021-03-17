91c14c5536e171c3a487d502e151f285d661685c
45software <50671522+45software@users.noreply.github.com>
2021-03-17 13:38:05 -0500

    Initial commit
    Created repository and added source files

---
+++ ConsoleApp/ConsoleService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+
+
+namespace ConsoleApp
+{
+	public class ConsoleService : IConsoleService
+	{
+		private readonly ILogger<ConsoleService> _logger;
+		private readonly IOptions<CustomOptions> _options;
+
+
+		public ConsoleService(ILogger<ConsoleService> logger,
+									IOptions<CustomOptions> options)
+		{
+			_logger = logger;
+			_options = options;
+		}
+
+
+		public void Run(string[] args)
+		{
+			_logger.LogInformation("The application is now starting {Start}", DateTime.Now);
+			Console.WriteLine($"The Url in the settings file is '{_options.Value.Url}'");
+		}
+	}
+}
---
+++ ConsoleApp/CustomOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp
+{
+	public class CustomOptions
+	{
+		public const string Section = "CustomSettings";
+		public string Url { get; set; }
+		public bool UseHttps { get; set; }
+	}
+}
