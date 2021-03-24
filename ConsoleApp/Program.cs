a60146af2b29b40fe0a902c32a80512f8079fb8f
Jon Krupa <sportstechnet@yahoo.com>
2021-03-24 09:40:20 -0500

    Added program.cs

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IHost host = Host.CreateDefaultBuilder()
+								.ConfigureServices((context, services) =>
+								{
+									services.Configure<CustomOptions>(context.Configuration
+																							.GetSection(CustomOptions.Section));
+
+									services.AddTransient<IConsoleService, ConsoleService>();
+
+									services.AddLogging();
+									services.AddOptions();
+								})
+								.Build();
+
+			IConsoleService service = ActivatorUtilities.CreateInstance<ConsoleService>(host.Services);
+			service.Run(args);
+		}
+	}
+}
